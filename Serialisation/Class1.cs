f2d43172c49a3746cf4b63bd7495be3dce7f6a60
unknown <fadwaelalaoui0@gmail.com>
2022-02-09 11:57:04 +0100

    firstcommit

---
+++ Serialisation/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Serialisation
+{
+    class Class1
+    {
+        public string nom { get; set; }
+        public string prenom { get; set; }
+        public int age { get; set; }
+
+        public Class1()
+        {
+
+        }
+        public Class1(string s, string p, int a)
+        {
+            nom = s;
+            prenom = p;
+            age = a;
+        }
+       public  void Afficher()
+        {
+            Console.WriteLine($"Nom: {this.nom}");
+            Console.WriteLine($"Prenom: {this.prenom}");
+            Console.WriteLine($"Age: {this.age}");
+        }
+    }
+}
---
+++ Serialisation/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
+namespace Serialisation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 elem = new Class1("Chaimae", "Elalaoui", 18);
+            Class1 elem2 = new Class1("Khadija", "Elalaoui", 68);
+            Class1 elem3 = new Class1("Leila", "Elalaoui", 80);
+
+            List<Class1> groupe = new List<Class1>();
+
+            groupe.Add(elem);
+            groupe.Add(elem2);
+            groupe.Add(elem3);
+            /***File Name**/
+            string Filename = "Content.json";
+            /**Serialisation**/
+            string jsonString = JsonSerializer.Serialize(groupe);
+
+           Console.WriteLine(jsonString);
+            /**Ecriture dans le fichier**/
+            File.WriteAllText(Filename, jsonString);
+            /**Affichage dans la console**/
+            Console.WriteLine(File.ReadAllText(Filename));
+
+            Console.WriteLine("--------------Deserialise-------------------");
+
+            string jsond = File.ReadAllText(Filename);
+
+            List<Class1> deserializze = JsonSerializer.Deserialize<List<Class1>>(jsond);
+
+
+            foreach(Class1 c in deserializze)
+            {
+                c.Afficher();
+            }
+
+
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
